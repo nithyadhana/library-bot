6cb450b3993db1e3599734036be65bd0d56a1874
nithyadhana <nithi1003@gmail.com>
2018-03-02 10:00:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,10 +34,10 @@ def makeWebhookResult(req):
         result = req.get("result")
         parameters = result.get("parameters")
         zone = parameters.get("subject_name")
-        csv_file = csv.reader(open('books.csv'), delimiter=",")
+        csv_file = csv.reader(open('books1.csv'), delimiter=",")
         
         for row in csv_file:
-            if zone == row[1]:
+            if zone == row[0]:
                 speech = ("Book Id: " + row[0] + "\n Book Title: " + row[1] + "\n Authors: " + row[2] + "\n Publication: " + row[3] + "\n Rack Number: "+ row[4])
 
         
